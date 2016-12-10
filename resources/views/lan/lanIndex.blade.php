49739c2d258074f5108cd3243caba6f9de791723
luxizhu <2324212163@qq.com>
2016-12-10 22:13:04 +0800

    lanindex

--- resources/views/lan/lanIndex.blade.php
+++ resources/views/lan/lanIndex.blade.php
@@ -1,4 +1,13 @@
 @extends('commPage\lanCommon')
 @section('lan')
-lan
+<div class="center-block" style="margin: 80px 0 0 50px;">
+    <div>
+    PHP基础<br/>
+        <a class="btn btn-primary" href="{{ URL('lan/varLearn') }}">变量</a>
+        <a class="btn btn-primary" href="#">表达式</a>
+        <a class="btn btn-primary" href="#">字符串</a>
+        <a class="btn btn-primary" href="#">控制结构</a>
+    </div>
+    <br/>
+</div>
 @endsection
