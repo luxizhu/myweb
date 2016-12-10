1b27952817225e8e415cefa99124704fafb7875b
luxizhu <2324212163@qq.com>
2016-12-10 21:18:55 +0800

    KnowController

---
+++ app/Http/Controllers/Know/KnowController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Know;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class KnowController extends Controller
+{
+    public function index(){
+        return view('lan.lanIndex');
+    }
+
+    public function create(){
+        echo 'create';
+    }
+
+    public function store(Request $request){
+        echo 'store';
+    }
+
+    public function show($id){
+        return $id;
+//        return view('main.detail',compact('id'));
+    }
+    public function edit($id){
+        echo 'edit';
+    }
+
+    public function update(Request $request, $id){
+        echo 'update';
+    }
+
+    public function destroy($id){
+        echo 'destroy';
+    }
+}
