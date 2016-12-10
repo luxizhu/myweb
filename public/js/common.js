387701f56aae7a5b549c26da9084416541e57b1c
luxizhu <2324212163@qq.com>
2016-12-10 20:23:46 +0800

    common js

--- public/js/common.js
+++ public/js/common.js
@@ -3,4 +3,9 @@ $(function(){
         var caret = "<b class=\"caret\"></b>";
         $("#lanNav ul li a:first").html($(this).text()+caret);
     });
+
+    $("#navList li").click(function(){
+        $("#navList li").removeClass("active");
+        $(this).addClass("active");
+    });
 });
