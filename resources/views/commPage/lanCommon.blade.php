610031cb25ddea52e49a884003a50009d5dc256b
luxizhu <2324212163@qq.com>
2016-12-10 20:15:11 +0800

    conmmon.js

--- resources/views/commPage/lanCommon.blade.php
+++ resources/views/commPage/lanCommon.blade.php
@@ -1,24 +1,26 @@
 @extends('common')
 @section('content')
-    <div id="content">
-        <div class="pull-left" style="width:15%">
-            <ul class="nav nav-pills">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                    PHP
-                        <b class="caret"></b>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">JAVA</a></li>
-                        <li><a href="#">Python</a></li>
-                        <li><a href="#">HTML</a></li>
-                        <li><a href="#">JS</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <div class="pull-left" style="width:80%;">
-            @yield('lan')
-        </div>
+    <div class="pull-left" style="width:100%" id="lanNav">
+        <ul class="nav nav-pills">
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                选择
+                    <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu" id="lanList">
+                    <li><a href="#">PHP</a></li>
+                    <li><a href="#">JAVA</a></li>
+                    <li><a href="#">Python</a></li>
+                    <li><a href="#">HTML</a></li>
+                    <li><a href="#">JS</a></li>
+                </ul>
+            </li>
+            <li id="nowTab" class="active">
+
+            </li>
+        </ul>
+    </div>
+    <div class="pull-left" style="width:80%;">
+        @yield('lan')
     </div>
 @endsection
