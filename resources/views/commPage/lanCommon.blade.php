4268b570da582b3ba9287ff1273e00c551fd9f66
luxizhu <2324212163@qq.com>
2016-12-12 09:58:03 +0800

    lanCommon

--- resources/views/commPage/lanCommon.blade.php
+++ resources/views/commPage/lanCommon.blade.php
@@ -1,6 +1,6 @@
 @extends('common')
 @section('content')
-    <div class="pull-left" style="width:100%" id="lanNav">
+    <div class="pull-left" id="lanNav">
         <ul class="nav nav-pills">
             <li class="dropdown">
                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">
@@ -8,11 +8,11 @@
                     <b class="caret"></b>
                 </a>
                 <ul class="dropdown-menu" id="lanList">
-                    <li><a href="#">PHP</a></li>
-                    <li><a href="#">JAVA</a></li>
-                    <li><a href="#">Python</a></li>
-                    <li><a href="#">HTML</a></li>
-                    <li><a href="#">JS</a></li>
+                    <li><a href="{{ URL('lan\php') }}">PHP</a></li>
+                    <li><a href="{{ URL('lan\java') }}">JAVA</a></li>
+                    <li><a href="{{ URL('lan\python') }}">Python</a></li>
+                    <li><a href="{{ URL('lan\html') }}">HTML</a></li>
+                    <li><a href="{{ URL('lan\js') }}">JS</a></li>
                 </ul>
             </li>
         </ul>
