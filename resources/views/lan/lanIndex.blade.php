f06591bf46bf4b3b0c289b380830930bb2c866ab
luxizhu <2324212163@qq.com>
2016-12-12 14:27:41 +0800

    lanIndex

--- resources/views/lan/lanIndex.blade.php
+++ resources/views/lan/lanIndex.blade.php
@@ -1,13 +1,30 @@
 @extends('commPage\lanCommon')
 @section('lan')
-<div class="center-block" style="margin: 80px 0 0 50px;">
-    <div>
-    PHP基础<br/>
-        <a class="btn btn-primary" href="{{ URL('lan/varLearn') }}">变量</a>
-        <a class="btn btn-primary" href="#">表达式</a>
-        <a class="btn btn-primary" href="#">字符串</a>
-        <a class="btn btn-primary" href="#">控制结构</a>
+    <div class="pull-left text-success btn btn-primary">
+        <span>{{ $course }}</span>
     </div>
-    <br/>
-</div>
+    @if($course=='php')
+        <div class="center-block" style="margin: 80px 0 0 50px;">
+            <div>
+                PHP基础{{ $course }}<br/>
+                <a class="btn btn-primary" href="{{ URL('php/varLearn') }}">变量</a>
+                <a class="btn btn-primary" href="#">表达式</a>
+                <a class="btn btn-primary" href="#">字符串</a>
+                <a class="btn btn-primary" href="#">控制结构</a>
+            </div>
+            <br/>
+        </div>
+    @elseif($course=='java')
+        <div class="center-block" style="margin: 80px 0 0 50px;">
+            java
+        </div>
+    @elseif($course=='python')
+        <div class="center-block" style="margin: 80px 0 0 50px;">
+            python
+        </div>
+    @else
+        <div class="center-block" style="margin: 80px 0 0 50px;">
+            null
+        </div>
+    @endif
 @endsection
