c441108ac3d43a7913cb76d56399fad0aab11097
luxizhu <2324212163@qq.com>
2017-04-12 14:46:36 +0800

    javascript.balde.php

---
+++ resources/views/main/javascript.blade.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Stock Quote From NASDAQ</title>
+    </head>
+    <body>
+        <?php
+        /*
+        echo 'Function sets supported in this install are:<br/>';
+        $extension = get_loaded_extensions();
+        if(is_array($extension) && !empty($extension)){
+            foreach ($extension as $each_ext){
+                echo "$each_ext<br/>";
+                echo '<ul>';
+                $ext_funcs = get_extension_funcs($each_ext);
+                if(is_array($ext_funcs) && !empty($ext_funcs)){
+                    foreach ($ext_funcs as $func){
+                        echo "<li>$func</li>";
+                    }
+                }
+                echo '</ul>';
+            }
+        echo get_current_user();
+        }*/
+        ?>
+    </body>
+</html>
